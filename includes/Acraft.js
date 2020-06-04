5c4153e7f77857ab6e9e4fb2cd382ed6d510d3f0
Mohsen <waltonmohsen@gmail.com>
2020-06-04 20:25:52 +0430

    check flags logic added

---
+++ includes/Acraft.js
@@ -0,0 +1,48 @@
+require("dotenv").config()
+const fs = require("fs")
+const chalk = require("chalk")
+/**
+ * main Acraft class
+ * @param httpPath
+ * apache 2 http path 
+ * @default "/var/www" Ubuntu
+ * @param vhostPath
+ * apache 2 vhost file path 
+ * @default "/etc/apache2/sites-available/000-default.conf" Ubuntu
+ * 
+ */
+module.exports = class Acraft {
+    constructor(httpPath = process.env.HTTP_PATH, vhostPath = process.env.VHOST_PATH) {
+        this.httpPath = httpPath
+        this.vhostPath = vhostPath
+    }
+    /**
+     * flags logic
+     * @param {*} flags 
+     */
+    checkFlags(flags) {
+        /**
+        * check for --folder-name and --url flags
+        */
+        if (flags.folderName == undefined || flags.url == undefined) {
+            console.log(chalk.red("please enter the folder name with -f or --folder-name parameter"))
+            console.log(chalk.red("please enter the site url with -u or --url"))
+            return
+        }
+        /**
+         * Check for --http-path
+         */
+        if (flags.httpPath == undefined) {
+            this.httpPath += flags.folderName
+            this.createSite(this.httpPath, flags.url)
+
+        } else {
+            this.httpPath = flags.httpPath + flags.folderName
+            this.createSite(this.httpPath,flags.url)
+        }
+    }
+
+    createSite(folderName, url) {
+        
+    }
+}
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+#!/usr/bin/node
 const fs = require('fs')
+const { Command } = require('commander')
+const chalk = require("chalk")
+const Acraft = require("./includes/Acraft")
+
+
+const program = new Command()
+const app = new Acraft()
+
+/**
+ * get the flags from command line and parse it
+ */
+program
+  .option("-f, --folder-name <value>", "site folder name")
+  .option("-u, --url <value>", "vhost url (example.local , test.com and etc ...)")
+  .option("-p, --http-path [value]", "www path to create folder (end with slash / or backslash \\ ex : home/www/)")
+  .option("-f, --vhost-path [value]", "path of apache 2 virtual host config file")
+  .parse(process.argv);
+
+
+
+app.checkFlags(program)
